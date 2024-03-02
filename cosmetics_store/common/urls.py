4e028398d38c9e64221648b1350766b724b896f7
Veselina S <vesi.as@abv.bg>
2024-03-02 20:03:48 +0200

    update templates

--- cosmetics_store/common/urls.py
+++ cosmetics_store/common/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from cosmetics_store.common.views import home_page, about
+
 urlpatterns = (
+    path("", home_page, name="home page"),
+    path("about/", about, name="about"),
 
 )
--- cosmetics_store/common/views.py
+++ cosmetics_store/common/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    context = {
+        "title": "home_page"
+    }
+
+    return render(request, "common/index.html", context)
+
+
+def about(request):
+    context = {}
+
+    return render(request, "common/about.html", context)
