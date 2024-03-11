1639c13567b6f3f5b13521645febe9ac3177d2a9
Veselina S <vesi.as@abv.bg>
2024-03-11 21:07:41 +0200

    update accounts views

--- cosmetics_store/accounts/views.py
+++ cosmetics_store/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import views as auth_views, get_user_model, login, logout
+from django.contrib.auth import mixins as auth_mixins
 from django.shortcuts import redirect
 from django.views import generic as generic_views
 from django.urls import reverse_lazy
@@ -14,10 +15,10 @@ class RegisterUserView(generic_views.CreateView):
     success_url = reverse_lazy("home page")
 
     def form_valid(self, form):
-        response = super().form_valid(form)
-        user = form.save()
-        login(self.request, user)  # log the user in after registration
-        return response
+        result = super().form_valid(form)
+        login(self.request, form.instance)  # log the user in after registration
+
+        return result
 
 
 class LoginUserView(auth_views.LoginView):
@@ -26,20 +27,22 @@ class LoginUserView(auth_views.LoginView):
     success_url = reverse_lazy("home page")
 
 
-class DetailsUserView(auth_views.LoginView):
+def logout_user(request):
+    logout(request)
+    return redirect("home page")
+
+
+class DetailsUserView(auth_mixins.LoginRequiredMixin, generic_views.DetailView):
+    model = UserModel
     template_name = "accounts/details_user.html"
 
 
-class UpdateUserView(auth_views.LoginView):
+class UpdateUserView(generic_views.UpdateView):
+    model = UserModel
     template_name = "accounts/edit_user.html"
     success_url = reverse_lazy("details user")
 
 
-class DeleteUserView(auth_views.LoginView):
+class DeleteUserView(generic_views.DeleteView):
     template_name = "accounts/edit_user.html"
     success_url = reverse_lazy("home page")
-
-
-def logout_user(request):
-    logout(request)
-    return redirect("home page")
