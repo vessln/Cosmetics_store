627371e31573b8f5d5a711b7654b487537a15d6e
Veselina S <vesi.as@abv.bg>
2024-04-07 15:15:06 +0300

    update css

--- static/js/remove_alert_messages.js
+++ static/js/remove_alert_messages.js
@@ -1,19 +1,3 @@
-// setTimeout(function() {
-//     const alertMessage = document.getElementById("alert-message");
-//     if (alertMessage) {
-//         let opacity = 1;
-//         const intervalId = setInterval(function() {
-//             opacity -= 0.03;
-//             alertMessage.style.opacity = opacity;
-//             if (opacity <= 0) {
-//                 clearInterval(intervalId); // Stop the interval when opacity reaches 0
-//                 alertMessage.style.display = "none"; // Hide the alert message
-//             }
-//         }, 30); // 30 milliseconds interval
-//     }
-// }, 3000); // 3000 milliseconds = 3 seconds
-
-
 setTimeout(function() {
     const alertMessages = document.querySelectorAll(".alert");
     alertMessages.forEach(function(alertMessage) {
